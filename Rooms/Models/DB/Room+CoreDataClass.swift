c65e30ab19dbd94bd556b885cb5c6b6cdaeafc1d
francescopaolodellaquila <paolodellaquila1999@gmail.com>
2021-12-05 16:29:41 +0100

    Complete SwiftUi Demo project

---
+++ Rooms/Models/DB/Room+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Room+CoreDataClass.swift
+//  MovieApp
+//
+//  Created by Francesco Paolo Dellaquila
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Room)
+public class Room: NSManagedObject {
+
+}
---
+++ Rooms/RoomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoomsApp.swift
+//  Rooms
+//
+//  Created by Francesco Paolo Dellaquila
+//
+
+import SwiftUI
+
+@main
+struct RoomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+           RoomListScreen() 
+        }
+    }
+}
